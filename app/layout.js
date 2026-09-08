1c4de4d2d737cc0896ab9e6be9654971d3e5229c
dasbappa0794-source <dasbappa0794@gmail.com>
2026-09-09 00:40:51 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import "./style.css";
+import { LanguageProvider } from "./i18n/LanguageContext";
 
 export const metadata = {
   title: "BuildNaro - House Construction Cost Calculator",
@@ -12,7 +14,9 @@ export default function RootLayout({ children }) {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8305568320429251" crossOrigin="anonymous"></script>
         <meta name="google-adsense-account" content="ca-pub-8305568320429251" />
       </head>
-      <body>{children}</body>
+      <body>
+        <LanguageProvider>{children}</LanguageProvider>
+      </body>
     </html>
   );
 }
